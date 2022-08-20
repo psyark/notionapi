13b6a3e052c83ee013b797b2485fbe1368bc2f38
keiichi <k@jue.jp>
2022-08-20 12:11:35 +0900

    いくつかのCoderをRawCoderに統合

--- codegen/coder.raw.go
+++ codegen/coder.raw.go
@@ -11,3 +11,15 @@ type RawCoder struct {
 func (f RawCoder) Code() jen.Code {
 	return f.Value
 }
+
+func AnonymousField(name string) RawCoder {
+	return RawCoder{jen.Id(name)}
+}
+
+func Comment(text string) RawCoder {
+	return RawCoder{jen.Comment(text)}
+}
+
+func CommentWithBreak(text string) RawCoder {
+	return RawCoder{jen.Comment(text).Line()}
+}
