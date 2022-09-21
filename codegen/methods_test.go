4b80e7abf7bf0f3c91cb62df58941a6bd199cfc8
keiichi <k@jue.jp>
2022-09-21 18:28:37 +0900

    methods_test整理

--- codegen/methods_test.go
+++ codegen/methods_test.go
@@ -4,35 +4,39 @@ import (
 	"testing"
 )
 
-func TestDatabaseMethods(t *testing.T) {
-	type Method struct {
-		DocURL  string
-		Returns string
-	}
-	methods := []Method{
-		{"https://developers.notion.com/reference/post-database-query", "PagePagination"},
-		{"https://developers.notion.com/reference/create-a-database", "Database"},
-		{"https://developers.notion.com/reference/update-a-database", "Database"},
-		{"https://developers.notion.com/reference/retrieve-a-database", "Database"},
-		{"https://developers.notion.com/reference/retrieve-a-page", "Page"},
-		{"https://developers.notion.com/reference/post-page", "Page"},
-		{"https://developers.notion.com/reference/patch-page", "Page"},
-		{"https://developers.notion.com/reference/retrieve-a-page-property", "PropertyItemOrPagination"},
-		{"https://developers.notion.com/reference/retrieve-a-block", "Block"},
-		{"https://developers.notion.com/reference/get-block-children", "BlockPagination"},
-		{"https://developers.notion.com/reference/patch-block-children", "BlockPagination"},
-		{"https://developers.notion.com/reference/delete-a-block", "Block"},
+func TestMethods(t *testing.T) {
+	methods := []*MethodCoder{
+		{DocURL: "https://developers.notion.com/reference/post-database-query", Returns: "PagePagination"},
+		{DocURL: "https://developers.notion.com/reference/create-a-database", Returns: "Database"},
+		{DocURL: "https://developers.notion.com/reference/update-a-database", Returns: "Database"},
+		{DocURL: "https://developers.notion.com/reference/retrieve-a-database", Returns: "Database"},
+		{DocURL: "https://developers.notion.com/reference/retrieve-a-page", Returns: "Page"},
+		{DocURL: "https://developers.notion.com/reference/post-page", Returns: "Page"},
+		{DocURL: "https://developers.notion.com/reference/patch-page", Returns: "Page"},
+		{DocURL: "https://developers.notion.com/reference/retrieve-a-page-property", Returns: "PropertyItemOrPagination"},
+		{DocURL: "https://developers.notion.com/reference/retrieve-a-block", Returns: "Block"},
+		{DocURL: "https://developers.notion.com/reference/get-block-children", Returns: "BlockPagination"},
+		{DocURL: "https://developers.notion.com/reference/patch-block-children", Returns: "BlockPagination"},
+		{DocURL: "https://developers.notion.com/reference/delete-a-block", Returns: "Block"},
 	}
 
 	builder := NewBuilder()
 
 	for _, method := range methods {
-		ssrProps, err := ParseMethod(method.DocURL)
-		if err != nil {
-			t.Fatal(err)
-		}
+		method := method
 
-		builder.Add(MethodCoder{DocURL: method.DocURL, Props: *ssrProps, Returns: method.Returns})
+		t.Run(method.DocURL, func(t *testing.T) {
+			// t.Parallel()
+
+			ssrProps, err := ParseMethod(method.DocURL)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			method.Props = *ssrProps
+		})
+
+		builder.Add(method)
 	}
 
 	if err := builder.Build("../client.methods.go"); err != nil {
