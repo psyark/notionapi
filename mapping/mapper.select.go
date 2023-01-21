1b1395d397e561eab6af793351e142567becaaf3
psyark <psyark@gmail.com>
2023-01-21 12:36:03 +0900

    mapper.select

--- mapping/mapper.select.go
+++ mapping/mapper.select.go
@@ -26,9 +26,40 @@ func (m *selectMapper) decodePage(value reflect.Value, page notionapi.Page) erro
 }
 
 func (m *selectMapper) createPageFrom(value reflect.Value, dst *notionapi.CreatePageOptions) error {
+	delta, err := m.getDelta(value, nil)
+	if err != nil {
+		return err
+	}
+	if delta != nil {
+		dst.Properties[m.propID] = *delta
+	}
 	return nil
 }
 
 func (m *selectMapper) updatePageFrom(value reflect.Value, page notionapi.Page, dst *notionapi.UpdatePageOptions) error {
+	delta, err := m.getDelta(value, m.getPropValue(page))
+	if err != nil {
+		return err
+	}
+	if delta != nil {
+		dst.Properties[m.propID] = *delta
+	}
 	return nil
+}
+
+func (m *selectMapper) getDelta(value reflect.Value, pv *notionapi.PropertyValue) (*notionapi.PropertyValue, error) {
+	if value.Kind() == reflect.String {
+		if value.String() == "" { // あれば消す
+			if pv != nil && pv.Select != nil {
+				return &notionapi.PropertyValue{Type: "select", Select: nil}, nil
+			}
+		} else { // 無ければ作る
+			if pv == nil || pv.Select == nil || pv.Select.Name != value.String() {
+				return &notionapi.PropertyValue{Type: "select", Select: &notionapi.SelectOption{Name: value.String()}}, nil
+			}
+		}
+		return nil, nil
+	} else {
+		return nil, fmt.Errorf("unsupported type: %v", reflect.TypeOf(value.Interface()))
+	}
 }
